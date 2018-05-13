cb36abc41162f0f63a8e8d9367d1809c6b452e3c
streetpoet@163.com <streetpoet@163.com>
2018-05-14 07:49:38 +0800

    update

--- src/main/java/com/spstudio/springsecuritydemo/config/HttpSecurityConfig.java
+++ src/main/java/com/spstudio/springsecuritydemo/config/HttpSecurityConfig.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -34,4 +38,72 @@ public class HttpSecurityConfig {
 		};
 	}
 
+	@Configuration
+	@Order(1)
+	public static class LoginConfig extends WebSecurityConfigurerAdapter {
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.antMatcher("/login")
+				.authorizeRequests()
+					.anyRequest()
+					.permitAll()
+					.and()
+				.formLogin();
+			// @formatter:on
+		}
+	}
+	
+	@Configuration
+	@Order(2)
+	public static class LoginConfig2 extends WebSecurityConfigurerAdapter {
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.antMatcher("/login2")
+				.authorizeRequests()
+					.anyRequest()
+					.permitAll()
+					.and()
+				.formLogin();
+			// @formatter:on
+		}
+	}
+
+	@Configuration
+	@Order(3)
+	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.antMatcher("/userSetting/**")
+				.authorizeRequests()
+					.anyRequest()
+					.hasRole("SPECIAL")
+					.and()
+				.formLogin();
+			// @formatter:on
+		}
+	}
+
+	@Configuration
+	@Order(4)
+	public static class FormLoginWebSecurityConfigurerAdapter2 extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.antMatcher("/userSetting2/**")
+				.authorizeRequests()
+					.anyRequest()
+					.authenticated()
+					.and()
+				.formLogin()
+					.loginPage("/login2");
+			// @formatter:on
+		}
+	}
 }
